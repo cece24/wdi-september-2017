f31aa0c4298f4a889fffd723b28f2d9de28f194f
Cecilia Wong <ceciliakhwong@gmail.com>
2017-09-19 12:13:46 -0400

    Data types and flow control lesson code

--- 06-programming-fundamentals-part-1/instructor/flow_control.rb
+++ 06-programming-fundamentals-part-1/instructor/flow_control.rb
@@ -5,16 +5,18 @@
 # (optionally) otherwise
 #   then do something else
 
+count = 11
 
+if count > 10
+  puts "count is greater than ten"
+elsif count < 10
+  puts "count is less than ten"
+else
+  puts "count is equal to ten"
+end
 
+puts "count is greater than ten" if count > 10
 
+puts "count is greater than ten" unless count <= 10
 
-# Looping
-
-# for loop
-# while loop
-# until loop
-
-
-
-# Iteration
+puts "End of program"
